a80ec53a200516fc52b556bd27c06e22889c84d3
SergeyMC9730 <bdsservers@yandex.ru>
2022-11-20 21:16:15 +0800

    RobTop features

--- incl/levels/setGJStars.php
+++ incl/levels/setGJStars.php
@@ -11,11 +11,21 @@ $gjp = ExploitPatch::remove($_POST["gjp"]);
 $stars = ExploitPatch::remove($_POST["stars"]);
 $feature = ExploitPatch::remove($_POST["feature"]);
 $levelID = ExploitPatch::remove($_POST["levelID"]);
+$silverCoins = ExploitPatch::remove($_POST["silverCoins"]);
 $accountID = GJPCheck::getAccountIDOrDie();
 $difficulty = $gs->getDiffFromStars($stars);
 
-if($gs->checkPermission($accountID, "actionSuggestRating")){
-	$gs->suggestLevel($accountID, $levelID, $difficulty["diff"], $stars, $feature, $difficulty["auto"], $difficulty["demon"]);
+// Make it to be boolean
+if($silverCoins > 1){
+	$silverCoins = 1;
+}else if ($silverCoins < 0){
+	$silverCoins = 0;
+}
+
+if($gs->checkPermission($accountID, "actionRateStars")){
+	$gs->rateLevel($accountID, $levelID, $stars, $difficulty["diff"], $difficulty["auto"], $difficulty["demon"]);
+	$gs->featureLevel($accountID, $levelID, $feature);
+	$gs->verifyCoinsLevel($accountID, $levelID, $silverCoins);
 	echo 1;
 }else{
 	echo -2;
