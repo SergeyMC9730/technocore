f21b89f1f7b61247253b36d3d0d0fb137d0cf3c2
SergeyMC9730 <bdsservers@yandex.ru>
2022-12-18 18:49:49 +0800

    fix issue

--- incl/levels/getOfficialLevels.php
+++ incl/levels/getOfficialLevels.php
@@ -6,12 +6,10 @@ $gameVersion = $_POST["gameVersion"];
 $has_custom_levels = 0;
 
 if(!is_numeric($gameVersion)) {
-    echo ("-2");
     exit("-2"); // Invalid game version
 }
 
 if($gameVersion != 21) {
-    echo ("-3");
     exit("-3"); // Old client
 }
 
@@ -19,10 +17,8 @@ if($has_custom_levels != 1) exit("-1"); // Check if levels are available, may be
 else {
     if(file_exists("../../data/GJLevelData.dat")){
         $leveldata = file_get_contents("../../data/GJLevelData.dat");
-        echo ($leveldata);
         exit($leveldata);
     } else {
-        echo ("-4");
         exit("-4"); // Does not exists
     }
 }
--- incl/misc/verifyPlayButtonAvailability.php
+++ incl/misc/verifyPlayButtonAvailability.php
@@ -3,11 +3,9 @@
 $has_custom_levels = 0;
 
 if($has_custom_levels != 1) {
-    echo ("-1");
     exit("-1");
 } // Not available
 else {
-    echo ("0");
     exit("0");
 }
 ?>
